3d521746f5db00afe7f4b22520ab7a61fd04f736
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-07-01 12:02:06 +0300

    delete: maven config files

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,10 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 
 import java.time.LocalDate;
 
@@ -16,7 +19,17 @@ public class Film {
     private int id;
     @NotNull
     private String name;
+    @Size(min = 1, max = 200)
     private String description;
     private LocalDate releaseDate;
+    @Positive
     private long duration;
+
+    public void setReleaseDate(LocalDate releaseDate, LocalDate checkDate) {
+        if (releaseDate.isBefore(checkDate)) {
+            throw new ValidationException("Incorrect data");
+        } else {
+            this.releaseDate = releaseDate;
+        }
+    }
 }
