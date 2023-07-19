f129ec54d3ab5eaa1fdf99c95da26ff341983c91
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2023-07-19 18:18:18 +0300

    refactor: удалил неиспользуемые импорты

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -15,15 +14,10 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class FilmService {
      private final FilmStorage filmStorage;
      private final UserStorage userStorage;
-
-     @Autowired
-     public FilmService(FilmStorage filmStorage, UserStorage userStorage) {
-        this.filmStorage = filmStorage;
-        this.userStorage = userStorage;
-     }
 
     public void like(Long filmId, Long userId) {
          Film film = filmStorage.getFilmById(filmId);
